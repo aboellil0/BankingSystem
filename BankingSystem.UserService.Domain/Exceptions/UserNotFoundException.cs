7ad7b20c6588dcf02c6dff79d06b4c88adac99f7
Mohamed Aboellil <mohamed.aboellil0@gmail.com>
2025-01-30 17:01:50 +0200

    dfgch

---
+++ BankingSystem.UserService.Domain/Exceptions/UserNotFoundException.cs
@@ -0,0 +1,20 @@
+﻿namespace BankingSystem.UserService.Domain.Exceptions
+{
+    internal class UserNotFoundException : UserServicesException
+    {
+        public string Identifier { get; set; }
+
+        public UserNotFoundException(string identifier) : base($"User {identifier} is not found")
+        {
+            Identifier = identifier;
+        }
+        public UserNotFoundException(string message, string identifier) : base(message)
+        {
+            Identifier = identifier;
+        }
+        public UserNotFoundException(string message, string identifier, Exception exciption) : base(message, exciption)
+        {
+            Identifier = identifier;
+        }
+    }
+}
---
+++ BankingSystem.UserService.Domain/Exceptions/UserServicesException.cs
@@ -0,0 +1,11 @@
+﻿namespace BankingSystem.UserService.Domain.Exceptions
+{
+    public class UserServicesException : Exception
+    {
+        public UserServicesException() { }
+
+        public UserServicesException(string message) : base(message) { }
+
+        public UserServicesException(string message, Exception innerException) : base(message, innerException) { }
+    }
+}
